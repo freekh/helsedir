e20e894c4723dc8621e842cab555253c05168ec0
Fredrik Ekholdt <freekh@gmail.com>
2014-02-27 16:35:15 +0100

    Added some command line options

--- code/src/main/scala/helsedir/DBConvert.scala
+++ code/src/main/scala/helsedir/DBConvert.scala
@@ -14,19 +14,46 @@ object DBConvert {
   def main(args: Array[String]) = {
     val dropBeforeInsert = true
     val dao = new DAO
-    
-    val typeMapFile = new File("../typemap.properties")
 
-    val typeMapping = TypeMap.read(typeMapFile)
+    args.headOption match {
+      case Some(importType) if importType == "activity" =>
+        args.take(2).drop(1).headOption.map(new File(_)) match {
+          case Some(activityFile) =>
+            val typeMapFilename = args.take(3).drop(2).headOption.getOrElse("../typemap.properties")
+            val typeMapFile = new File(typeMapFilename)
 
-    //val activityFile = new File("../data/2013_12_aktivitet_HF_NASJ.txt")
-    val activityFile = new File("../data/2011_12_aktivitet_HF_NASJ.txt")
-    val activity = new ActivityData(activityFile, typeMapping)
-    TableCreator.createTable(activity, dao, dropBeforeInsert = dropBeforeInsert)
+            if (!typeMapFile.isFile) {
+              System.err.println("Typemap: " + typeMapFile.getAbsolutePath + " is not a file")
+            } else if (!activityFile.isFile) {
+              System.err.println("Activity data: " + activityFile.getAbsolutePath + " is not a file")
+            } else if (typeMapFile.isFile && activityFile.isFile) {
+              val typeMapping = TypeMap.read(typeMapFile)
+              val activity = new ActivityData(activityFile, typeMapping)
+              TableCreator.createTable(activity, dao, dropBeforeInsert = dropBeforeInsert)
+            } else {
+              throw new Exception("Unknown state!")
+            }
+          case None =>
+            System.err.println("Please specify activity data filename")
+        }
+      case Some(importType) if importType == "ref" =>
+        args.take(2).drop(1).headOption.map(new File(_)) match {
+          case Some(refTableFile) =>
+            if (refTableFile.isFile) {
+              val refTable = new ReferenceTable(refTableFile)
+              TableCreator.createTable(refTable, dao, dropBeforeInsert = dropBeforeInsert)
+            } else {
+              System.err.println("Ref table: " + refTableFile.getAbsolutePath + " is not a file")
 
-    val refTable = new ReferenceTable(new File("../data/Referansetabell_2013 20 juni.xlsx"))
-
-//    TableCreator.createTable(refTable, dao, dropBeforeInsert = dropBeforeInsert)
-    
+            }
+          case None =>
+            System.err.println("Please specify ref table filename")
+        }
+      case Some(importType) => {
+        System.err.println(s"Unknown import type '$importType'. Valid types: activity, ref")
+      }
+      case None =>
+        System.err.println("Import-type required! Valid types: activity, ref")
+    }
   }
 }
